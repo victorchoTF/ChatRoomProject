09ff396950906a5e9c245ec91f3fc391271b21ea
victorchotf <vrgeorgiev06@gmail.com>
2024-02-09 15:08:48 +0200

    home form init

---
+++ main.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_socketio import join_room, leave_room, send, SocketIO
+import random
+from string import ascii_uppercase
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "h3fhVd2dDn8DcS"
+socketio = SocketIO(app)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
